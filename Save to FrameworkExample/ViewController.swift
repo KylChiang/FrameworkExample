74da097abc8fafb69b14c257b08c1af8ac120d97
Kylie <kylie.chiang@ecpay.com.tw>
2020-10-29 15:11:03 +0800

    Add the target B & UI

---
+++ Save to FrameworkExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Save to FrameworkExample
+//
+//  Created by Kylie on 2020/10/29.
+//  Copyright © 2020 Opay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
