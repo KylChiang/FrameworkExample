6a08020aefe939679341068dabd32a9962058a5a
Kylie <kylie.chiang@ecpay.com.tw>
2020-10-29 15:18:54 +0800

    Add the MNParentViewController framework

---
+++ MNParentViewController/MNParentViewController.h
@@ -0,0 +1,19 @@
+//
+//  MNParentViewController.h
+//  MNParentViewController
+//
+//  Created by Kylie on 2020/10/29.
+//  Copyright © 2020 Opay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MNParentViewController.
+FOUNDATION_EXPORT double MNParentViewControllerVersionNumber;
+
+//! Project version string for MNParentViewController.
+FOUNDATION_EXPORT const unsigned char MNParentViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MNParentViewController/PublicHeader.h>
+
+
