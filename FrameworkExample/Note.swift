c9674b9179323184cd5ca2d9c30b204e2730fd68
Kylie <kylie.chiang@ecpay.com.tw>
2020-10-29 15:06:12 +0800

    Add framework data struct

---
+++ FrameworkExample/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  FrameworkExample
+//
+//  Created by Kylie on 2020/10/29.
+//  Copyright © 2020 Opay. All rights reserved.
+//
+
+import Foundation
+
+struct Note {
+    var title = ""
+}
