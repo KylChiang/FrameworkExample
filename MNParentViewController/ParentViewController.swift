46a11a9456dcf59e706f8ca1da832c17b74fd2f6
Kylie <kylie.chiang@ecpay.com.tw>
2020-11-17 17:10:24 +0800

    Add MyStaticLib & remove PrivateAlamofire reference

--- MNParentViewController/ParentViewController.swift
+++ MNParentViewController/ParentViewController.swift
@@ -7,24 +7,14 @@
 //
 
 import UIKit
+import MyStaticLib
 
 public class ParentViewController: UIViewController {
 
     override public func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        MyStaticLib.functionA()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
