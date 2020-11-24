3f28972a9ecf2e1513c5cff704ae7d478acae5db
Kylie <kylie.chiang@ecpay.com.tw>
2020-11-24 10:40:14 +0800

    Add pods function & gen XCFramework file

--- MNParentViewController/ParentViewController.swift
+++ MNParentViewController/ParentViewController.swift
@@ -7,6 +7,14 @@
 //
 
 import UIKit
+import Alamofire
+import PromiseKit
+
+public enum APIError: Error {
+    case decodeFail
+    case parseFail
+    case returnError(code: String , msg :String)
+}
 
 public class ParentViewController: UIViewController {
 
@@ -16,15 +24,48 @@ public class ParentViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    static let api_url:String = "http://httpbin.org/post"
+    
+    public static func queryPostAPI() -> Promise<[String: Any]> {
+            return Promise{ seal in
+                
+                let paras = ["num":"10" , "str":"This is String"]
+                AF.request(api_url,
+                           method: .post,
+                           parameters: paras,
+                           encoder: URLEncodedFormParameterEncoder.default){ urlRequest in
+                            urlRequest.timeoutInterval = 10
+    //                           urlRequest.allowsConstrainedNetworkAccess = false
+                           }
+                    .validate(statusCode: 200..<300)
+    //                .validate(statusCode: 300..<400)
+                    .responseData{ response in
+                        
+                    debugPrint(response)
+                        switch response.result {
+                        case .success(let data):
+                            
+                            do {
+                                // make sure this JSON is in the format we expect
+                                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                                    // try to read out a string array
+                                    return seal.fulfill(json)
+                                }
+                                
+                            } catch let error as NSError {
+                                print("Failed to load: \(error.localizedDescription)")
+                                seal.reject(APIError.parseFail)
+                            }
+                            seal.reject(APIError.parseFail)
+                            
+                        case .failure(let error):
+    //                        print(error)
+                            seal.reject(error)
+                        }
+                        
+                            
+                }
+            }
+        }
 
 }
